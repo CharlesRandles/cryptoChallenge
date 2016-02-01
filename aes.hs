59b7f3685872b7308e8e8569b84d20ecf6bc92cd
Charles Randles <charles.randles@gmail.com>
2016-02-02 08:28:56 +1000

    aes.hs is now a key library

---
+++ aes.hs
@@ -0,0 +1,52 @@
+module AES (ecbEncrypt,
+            ecbDecrypt,
+            cbcEncrypt,
+            cbcDecrypt) where
+
+import Crypto.Cipher.AES
+import Data.ByteString.Char8 (pack, unpack)
+import Data.ByteString as B (ByteString,
+                             take,
+                             drop,
+                             null,
+                             empty,
+                             append)
+import Data.Bits (xor)
+
+import CryptoChallenge
+import XorEncode
+import PKCS7
+
+toByteString :: Cipher -> B.ByteString
+toByteString  c = pack (toString c)
+
+fromByteString :: B.ByteString -> Cipher
+fromByteString = fromString . unpack
+
+ecbEncrypt = encryptECB
+ecbDecrypt = decryptECB
+
+cbcEncrypt :: AES -> ByteString -> ByteString -> ByteString
+cbcEncrypt key vector plaintext
+        | B.null plaintext = B.empty
+        | otherwise = let cipher = encryptECB 
+                                       key 
+                                       (xorByteString 
+                                           vector 
+                                           (B.take aesBlocksize plaintext)) in
+                             B.append cipher (cbcEncrypt key cipher (B.drop aesBlocksize plaintext))
+
+cbcDecrypt :: AES -> ByteString -> ByteString -> ByteString
+cbcDecrypt key vector ciphertext
+           | B.null ciphertext = B.empty
+           | otherwise = let cipherblock = (B.take aesBlocksize ciphertext) in
+                             B.append 
+                               (xorByteString (decryptECB key cipherblock) vector)
+                               (cbcDecrypt key cipherblock (B.drop aesBlocksize ciphertext))
+                             
+
+key = initAES $ pack "YELLOW SUBMARINE"
+vector = pack $ toString $  replicate aesBlocksize 0
+
+plaintext = pack $ toString $ pkcs7 (fromString "Now is the winter of our discontent - made glorious summer by this son of York. ") aesBlocksize
+
